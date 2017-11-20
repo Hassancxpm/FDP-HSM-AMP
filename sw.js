5e7ace30a8f33bed72330c677d6b05087c8aae42
hassan cxpm <hassan.akaou@comexposium.com>
2017-11-20 17:33:27 +0100

    Correction du SW

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/sw-toolbox/3.6.1/sw-toolbox.js');
 
-toolbox.precache(['index.html','favicon.png','logo.jpg', 'plan.jpg', 'lampadaire.jpg']);
+toolbox.precache(['index.html','favicon.png','logo.webp', 'plan.webp', 'lampadaire.webp']);
 
 toolbox.router.default = toolbox.cacheFirst;
 
